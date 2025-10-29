68f811566f25ab1336a58a9d1c431ed69a8eb5a5
Ziad Nashaat <ziadnashaat03@gmail.com>
2025-10-29 16:00:32 +0200

    fixed some conflicts

--- models/tourModel.js
+++ models/tourModel.js
@@ -103,7 +103,6 @@ const tourSchema = new Schema(
         day: Number,
       },
     ],
-    guides: Array,
   },
   {
     toJSON: { virtuals: true },
@@ -118,13 +117,6 @@ tourSchema.virtual('durationWeeks').get(function () {
 // DOCUMENT MIDDLEWARE: runs before .save() and .create()
 tourSchema.pre('save', function (next) {
   this.slug = slugify(this.name, { lower: true });
-  next();
-});
-
-tourSchema.pre('save', async function (next) {
-  const guidesPromises = this.guides.map(async id => await User.findById(id));
-  this.guides = await Promise.all(guidesPromises);
-
   next();
 });
 
