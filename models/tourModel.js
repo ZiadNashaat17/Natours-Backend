4e1d168160ad37a42297b7702dde95f8252453d1
Ziad Nashaat <ziadnashaat03@gmail.com>
2025-10-29 16:18:16 +0200

    Embedding tour guides in tour model

--- models/tourModel.js
+++ models/tourModel.js
@@ -103,6 +103,7 @@ const tourSchema = new Schema(
         day: Number,
       },
     ],
+    guides: Array,
   },
   {
     toJSON: { virtuals: true },
@@ -117,6 +118,13 @@ tourSchema.virtual('durationWeeks').get(function () {
 // DOCUMENT MIDDLEWARE: runs before .save() and .create()
 tourSchema.pre('save', function (next) {
   this.slug = slugify(this.name, { lower: true });
+  next();
+});
+
+tourSchema.pre('save', async function (next) {
+  const guidesPromises = this.guides.map(async id => await User.findById(id));
+  this.guides = await Promise.all(guidesPromises);
+
   next();
 });
 
