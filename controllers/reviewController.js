ed1531d43b67b4b399f3e36bb0666c3b86c40cd7
Ziad Nashaat <ziadnashaat03@gmail.com>
2025-10-30 17:09:05 +0200

    Nested Get Endpoint

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -1,8 +1,12 @@
 import Review from '../models/reviewModel.js';
+import AppError from '../utils/appError.js';
 import catchAsync from '../utils/catchAsync.js';
 
 export const getAllReviews = catchAsync(async (req, res, next) => {
-  const reviews = await Review.find();
+  let filter = {};
+  if (req.params.tourId) filter = { tour: req.params.tourId };
+
+  const reviews = await Review.find(filter);
 
   res.status(200).json({
     status: 'success',
