30c456faafe3ea862380b54b9e52d42fee21ea14
Ziad Nashaat <ziadnashaat03@gmail.com>
2025-10-30 11:34:24 +0200

    Creating Review Model

---
+++ models/reviewModel.js
@@ -0,0 +1,46 @@
+import { Schema, model } from 'mongoose';
+
+const reviewSchema = new Schema(
+  {
+    review: {
+      type: String,
+      required: [true, 'Review cannot be empty!!'],
+      trim: true,
+    },
+    rating: {
+      type: Number,
+      required: [true, 'Review must have a rating'],
+      min: 1,
+      max: 5,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+    },
+    tour: {
+      type: Schema.ObjectId,
+      ref: 'Tour',
+      required: [true, 'Review msut belong to a tour.'],
+    },
+    user: {
+      type: Schema.ObjectId,
+      ref: 'User',
+      required: [true, 'Review must belong to a user'],
+    },
+  },
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  }
+);
+
+reviewSchema.pre(/^find/, function (next) {
+  this.populate('Tour');
+  this.populate('User');
+
+  next();
+});
+
+const Review = model('Review', reviewSchema);
+
+export default Review;
