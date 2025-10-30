e05880bceb363c12f2cc5e474e15fbdb712a7c63
Ziad Nashaat <ziadnashaat03@gmail.com>
2025-10-30 14:47:10 +0200

    Created Reviews and Managed it

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ import AppError from './utils/appError.js';
 import globalErrorHandler from './controllers/errorController.js';
 import tourRouter from './routes/tourRouter.js';
 import userRouter from './routes/userRouter.js';
+import reviewRouter from './routes/reviewRouter.js';
 
 const app = express();
 
@@ -65,6 +66,7 @@ app.use((req, res, next) => {
 // 3. Routes
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+app.use('/api/v1/reviews', reviewRouter);
 
 app.all('*', (req, res, next) => {
   next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
