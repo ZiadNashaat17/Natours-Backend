7744fb8e731b6f2dcdc4cc8ac8154bb6080e733e
Ziad Nashaat <ziadnashaat03@gmail.com>
2025-10-30 08:33:59 +0200

    Child referencing

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,6 +1,6 @@
 import { Schema, model } from 'mongoose';
 import slugify from 'slugify';
-import User from './userModel.js';
+// import User from './userModel.js';
 // import validator from 'validator';
 
 const tourSchema = new Schema(
@@ -103,7 +103,12 @@ const tourSchema = new Schema(
         day: Number,
       },
     ],
-    guides: Array,
+    guides: [
+      {
+        type: Schema.ObjectId,
+        ref: 'User',
+      },
+    ],
   },
   {
     toJSON: { virtuals: true },
@@ -121,12 +126,12 @@ tourSchema.pre('save', function (next) {
   next();
 });
 
-tourSchema.pre('save', async function (next) {
-  const guidesPromises = this.guides.map(async id => await User.findById(id));
-  this.guides = await Promise.all(guidesPromises);
+// tourSchema.pre('save', async function (next) {
+//   const guidesPromises = this.guides.map(async id => await User.findById(id));
+//   this.guides = await Promise.all(guidesPromises);
 
-  next();
-});
+//   next();
+// });
 
 // tourSchema.post('save', function (doc, next) {
 //   console.log(doc);
